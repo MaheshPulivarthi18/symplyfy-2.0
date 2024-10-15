a413cdf29de39e05290e299f3adea943b3feb3d9
Puliuvarthi.Mahesh <134507390+MaheshPulivarthi18@users.noreply.github.com>
2024-10-15 14:51:19 +0530

    add invoiceprefix123

--- src/components/clinic/ClinicInformation.jsx
+++ src/components/clinic/ClinicInformation.jsx
@@ -131,87 +131,9 @@ const ClinicInformation = () => {
     setIsPrefixPatientEditable(true);
   };
 
-  const fetchWithTokenHandling = async (url, options = {}) => {
-    try {
-      const response = await authenticatedFetch(url, options);
-      if (!response.ok) {
-        const errorData = await response.json();
-        throw new Error(errorData.detail || 'An error occurred');
-      }
-      return response.json();
-    } catch (error) {
-      if (error.message === 'Token is blacklisted' || error.message === 'Token is invalid or expired') {
-        navigate('/login');
-        throw new Error('Session expired. Please log in again.');
-      }
-      throw error;
-    }
-  };
-
-  const handleFileChange = (event) => {
-    const file = event.target.files[0];
-    // setSelectedFile(file);
-    if (file && file.type.startsWith('image/')) {
-      setSelectedFile(file);
-    } else {
-      toast({ title: "Error", description: "Please select a valid image file.", variant: "destructive" });
-      event.target.value = null; // Reset the input
-    }
-  };
-
-  const uploadLogo = async () => {
-    if (!selectedFile) {
-      toast({ title: "Error", description: "Please select a file to upload.", variant: "destructive" });
-      return false;
-    }
-    // console.log("Requesting presigned URL...");
-    const presignedUrlResponse = await fetchWithTokenHandling(
-      `${import.meta.env.VITE_BASE_URL}/api/emp/clinic/${clinic_id}/logo/`,
-      {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ "logo_long_filename": selectedFile.name }),
-      }
-    );
-    
-    const { logo_long_filename: fileName, logo_long: presignedUrl } = presignedUrlResponse;
-    
-    // console.log("Uploading file...");
-    const formData = new FormData();
-    formData.append('file', selectedFile);
-    console.log(selectedFile)
-    
-    // console.log("Presigned URL received:", presignedUrl);
-    await axios.put(presignedUrl, selectedFile, {
-      headers: {
-        'Content-Type': "multipart/form-data",
-        'Content-Length': selectedFile.size,
-        "x-amz-acl": "public-read" 
-      },
-    });
-
-    // console.log("File uploaded successfully. Marking as completed...");
-    await fetchWithTokenHandling(
-      `${import.meta.env.VITE_BASE_URL}/api/emp/clinic/${clinic_id}/logo/complete/`,
-      {
-        method: 'PATCH',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ "logo_long_completed": true }),
-      }
-    );
-
-    // console.log("File upload process completed successfully.");
-    toast({ title: "Success", description: "File uploaded successfully" });
-    setSelectedFile(null);
-    await fetchClinicData();
-    return true;
-
-  }
-
   if (isLoading) {
     return <div>Loading...</div>;
   }
-
 
   return (
     <Card className="w-full max-w-4xl mx-auto mt-8">
@@ -219,26 +141,13 @@ const ClinicInformation = () => {
         <CardTitle className="text-2xl font-bold">Clinic Information</CardTitle>
       </CardHeader>
       <CardContent>
-      <p className=' font-semibold'>
-          Invoice Logo
-        </p>
+        <p className='font-semibold'>Invoice Logo</p>
         <div className='flex justify-center'>
-          {presentImg !== null ? (
-            <img src={presentImg} alt={"Invoice Logo"} className="max-w-full max-h-[80vh] object-contain" />
-            ) : 
-            (<>No logo Uploaded Yet</>)
-          }
-        </div>
-        <div className='flex gap-6'>
-          <Input 
-            type="file"
-            accept="image/*"
-            onChange={handleFileChange} 
-          />
-          <Button onClick={() => {
-            uploadLogo()}}>
-            Update Logo
-          </Button>
+          {presentImg ? (
+            <img src={presentImg} alt="Invoice Logo" className="max-w-full max-h-[80vh] object-contain" />
+          ) : (
+            <>No logo uploaded yet</>
+          )}
         </div>
         <Form {...form}>
           <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
